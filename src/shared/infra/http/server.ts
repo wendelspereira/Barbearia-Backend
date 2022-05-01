fc7e49d2ad9c83498032ebee610abcd6cfe44172
Wendel S. Pereira <netalcode@gmail.com>
2022-04-30 23:14:32 -0300

    sds1

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,4 +7,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env || 3333, () => console.log("Server is running!"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running!"));

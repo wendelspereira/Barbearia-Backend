d05354f70ab5d366425a4fa6431823ed9154a465
Wendel S. Pereira <netalcode@gmail.com>
2022-05-25 16:32:49 -0300

    Heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,9 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { routes } from "../http/routes";
 import { AppError } from "../../errors/AppError";
-// import createConnection from "../typeorm";
-dotenv.config();
-// createConnection();
-import '../typeorm'
+import "../typeorm";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,34 +1,32 @@
-import { AppError } from "../../errors/AppError";
 import { createConnection, Connection } from "typeorm";
+import { AppError } from "../../errors/AppError";
 require("dotenv").config();
 
-export default async (): Promise<Connection> => {
-  const rootDir = process.env.NODE_ENV === "development" ? "src" : "dist/src";
-  const extensionFile = process.env.NODE_ENV === "development" ? "ts" : "js";
+const rootDir = process.env.NODE_ENV === "development" ? "src" : "dist/src";
+const extensionFile = process.env.NODE_ENV === "development" ? "ts" : "js";
 
-  const config: any = {
-    type: "postgres",
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+const config: any = {
+  type: "postgres",
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
 
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
     },
+  },
 
-    entities: [`${rootDir}/**/**.entity.${extensionFile}`],
-    migrations: [`${rootDir}/**/migrations/*.${extensionFile}`],
-    cli: {
-      migrationsDir: `${rootDir}/shared/infra/typeorm/migrations`,
-    },
-  };
-
-  return createConnection(config).catch((err) => {
-    throw new AppError("Error on connect to database" + err);
-  });
+  entities: [`${rootDir}/**/**.entity.${extensionFile}`],
+  migrations: [`${rootDir}/**/migrations/*.${extensionFile}`],
+  cli: {
+    migrationsDir: `${rootDir}/shared/infra/typeorm/migrations`,
+  },
 };
+
+console.log(`${rootDir}/**/**.entity.${extensionFile}`);
+
+createConnection(config).catch((err) => console.log(err));

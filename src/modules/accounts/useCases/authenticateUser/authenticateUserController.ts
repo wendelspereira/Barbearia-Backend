b39c5d89b718a1f58bee5e382893552bf21893c5
Wendel S. Pereira <netalcode@gmail.com>
2022-06-01 16:57:58 -0300

    _

--- src/modules/accounts/useCases/authenticateUser/authenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserController.ts
@@ -3,6 +3,8 @@ import { AuthenticateUseCase } from "./authenticateUserUseCase";
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response): Promise<Response>{
+console.log(request.body)
+
     const { email, password } = request.body;
     const authenticateUseCase = new AuthenticateUseCase();
     const result = await authenticateUseCase.execute({ email, password });
